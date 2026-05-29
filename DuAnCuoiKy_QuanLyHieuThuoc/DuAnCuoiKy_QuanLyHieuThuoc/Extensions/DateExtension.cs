6be9a0e7c27e0bfeaa866acc957e2a79320a59d6
Tex-LeTrongTin <trongtin23102006@gmail.com>
2026-05-30 02:40:52 +0700

    Bổ sung các thành phẩn sử dụng chung cho dự án

---
+++ DuAnCuoiKy_QuanLyHieuThuoc/DuAnCuoiKy_QuanLyHieuThuoc/Extensions/DateExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DuAnCuoiKy_QuanLyHieuThuoc.Extensions
+{
+    public static class DateExtension
+    {
+        // Tính số ngày đến hạn dùng
+        public static int TinhSoNgayDenHan(this DateTime hanSuDung)
+        {
+            TimeSpan diff = hanSuDung.Date - DateTime.Today;
+            return diff.Days;
+        }
+    }
+}
